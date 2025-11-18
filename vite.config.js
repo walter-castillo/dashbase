1bbd3a77c66b7caea684100e29e0d4688b55b0db
Walter Castillo <waltercastillo@live.com.ar>
2025-11-18 17:57:28 -0300

    modifiqueviteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,30 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        }
+      }
+    }
+  },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3001,
   },
   watch: {
     usePolling: true,
   },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      jspdf: "jspdf/dist/jspdf.umd.js",
+    },
+  },
+});
+
+
